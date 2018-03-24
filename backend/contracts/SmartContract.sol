ebfd861cecbe55cc406be34b2479e107c13aa3de
johannesridho <johanridho@gmail.com>
2018-03-24 16:13:01 +0700

    add smart contract

---
+++ backend/contracts/SmartContract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.21;
+
+contract SmartContract {
+    string public standard = 'SmartContract 0.1';
+
+    struct Ownership {
+        uint houseId;
+        bytes32 data;
+    }
+
+    mapping (address => Ownership[]) public ownerships;
+
+    function SmartContract() {}
+
+    function addOwnerships(uint houseId, bytes32 data) {
+        ownerships[msg.sender].push(Ownership({
+            houseId: houseId,
+            data: data
+        }));
+    }
+
+    function getOwnerships() public constant returns (Ownership[]) {
+        return ownerships[msg.sender];
+    }
+}
