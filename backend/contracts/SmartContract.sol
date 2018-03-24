bde2ef19d4fe1b37d1eb7355195f0a3e5f03db72
johannesridho <johanridho@gmail.com>
2018-03-24 23:49:11 +0700

    fix contract methods

--- backend/contracts/SmartContract.sol
+++ backend/contracts/SmartContract.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.4.21;
-pragma experimental ABIEncoderV2;
 
 contract SmartContract {
     string public standard = 'SmartContract 0.1';
@@ -37,8 +36,17 @@ contract SmartContract {
         }));
     }
 
-    function getOwnerships() public constant returns (Ownership[]) {
-        return ownerships[msg.sender];
+    function getOwnerships(uint[] indexes) public returns (uint[], bytes32[]) {
+        uint[] memory houseIds = new uint[](indexes.length);
+        bytes32[] memory dataArray = new bytes32[](indexes.length);
+
+        for (uint i = 0; i < indexes.length; i++) {
+            Ownership storage ownership = ownerships[msg.sender][indexes[i]];
+            houseIds[i] = ownership.houseId;
+            dataArray[i] = ownership.data;
+        }
+
+        return (houseIds, dataArray);
     }
 
     function addSubmission(uint houseId, bytes32 status, uint256 price, uint256 monthlyInstallment) {
@@ -50,8 +58,21 @@ contract SmartContract {
         }));
     }
 
-    function getSubmissions() public constant returns (Submission[]) {
-        return submissions[msg.sender];
+    function getSubmissions(uint[] indexes) public constant returns (uint[], bytes32[], uint256[], uint256[]) {
+        uint[] memory houseIds = new uint[](indexes.length);
+        bytes32[] memory statuses = new bytes32[](indexes.length);
+        uint256[] memory prices = new uint[](indexes.length);
+        uint256[] memory monthlyInstallments = new uint[](indexes.length);
+
+        for (uint i = 0; i < indexes.length; i++) {
+          Submission storage submission = submissions[msg.sender][indexes[i]];
+          houseIds[i] = submission.houseId;
+          statuses[i] = submission.status;
+          prices[i] = submission.price;
+          monthlyInstallments[i] = submission.monthlyInstallment;
+        }
+
+        return (houseIds, statuses, prices, monthlyInstallments);
     }
 
     function addTransaction(address accountAddress, uint256 amount, uint houseId, uint256 remainingDebt, uint256 price) {
