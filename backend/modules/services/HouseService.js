663af1fc5c15484980573c0aef50b2cac85ec2bf
Ananta Pandu Wicaksana <anpandumail@gmail.com>
2018-03-24 21:39:31 +0700

    [backend] house; remove unused properties

--- backend/modules/services/HouseService.js
+++ backend/modules/services/HouseService.js
@@ -13,10 +13,37 @@ HouseService.prototype.transform = (house) => {
     newHouse.gbr4,
     newHouse.gbr5,
   ]
-  newHouse.propinsi = (newHouse.id_propinsi == 31) ? 'dki jakarta' : 'dki jakarta'
-  newHouse.kab_kota = (newHouse.id_kab_kota == 3174) ? 'jakarta selatan' : 'jakarta pusat'
-  newHouse.kecamatan = (newHouse.id_kecamatan == 317408) ? 'pancoran' : 'sawah besar'
-  newHouse.desa_kelurahan = (newHouse.id_desa_kelurahan == 3174081002) ? 'kalibata' : 'gunung sahari utara'
+  newHouse.propinsi = (newHouse.id_propinsi == 31) ? 'DKI Jakarta' : 'DKI Jakarta'
+  newHouse.kab_kota = (newHouse.id_kab_kota == 3174) ? 'Jakarta selatan' : 'Jakarta pusat'
+  newHouse.kecamatan = (newHouse.id_kecamatan == 317408) ? 'Pancoran' : 'Sawah Besar'
+  newHouse.desa_kelurahan = (newHouse.id_desa_kelurahan == 3174081002) ? 'Kalibata' : 'Gunung Sahari Utara'
+  delete newHouse.gbr1
+  delete newHouse.gbr2
+  delete newHouse.gbr3
+  delete newHouse.gbr4
+  delete newHouse.gbr5
+  delete newHouse.id_propinsi
+  delete newHouse.id_kab_kota
+  delete newHouse.id_kecamatan
+  delete newHouse.id_desa_kelurahan
+  delete newHouse.keyword
+  delete newHouse.tgl_insert
+  delete newHouse.jenis_lama
+  delete newHouse.subsidi_lama
+  delete newHouse.jalur_pdam_lama
+  delete newHouse.jalur_telepon_lama
+  delete newHouse.status_unit_lama
+  delete newHouse.status_pengajuan_lama
+  delete newHouse.video
+  delete newHouse.tgl_update_status
+  delete newHouse.last_update
+  delete newHouse.status_eloan
+  delete newHouse.respon_eloan
+  delete newHouse.tgl_kirim_eloan
+  delete newHouse.kategori_agunan
+  delete newHouse.jenis_lelang
+  delete newHouse.created_at
+  delete newHouse.updated_at
   return newHouse
 }
 
